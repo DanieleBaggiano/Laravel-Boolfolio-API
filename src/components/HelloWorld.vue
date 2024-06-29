ed49f4a3f5d04539ec1673a465855ebac23c169d
DanieleBaggiano <danielebaggiano@gmail.com>
2024-06-29 16:54:07 +0200

    Milestone 4

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,24 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      ProjectCard: [],
+    }
+  },
+  created() {
+    axios.get("http://localhost:8000/api/users").then(Response => {
+      this.ProjectCard = Response.data.users;
+      console.log(this.ProjectCard);
+    })
+  },
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div>
+    miao
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<style lang="scss" scoped></style>

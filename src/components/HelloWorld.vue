ead0d7ae2e046ab6a9300a45914ff64133c0d216
DanieleBaggiano <danielebaggiano@gmail.com>
2024-06-30 12:57:01 +0200

    Milestone 5

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,21 +3,29 @@ import axios from "axios";
 export default {
   data() {
     return {
+      UserCard: [],
       ProjectCard: [],
     }
   },
   created() {
     axios.get("http://localhost:8000/api/users").then(Response => {
-      this.ProjectCard = Response.data.users;
+      this.UserCard = Response.data.results;
+      console.log(this.UserCard);
+    });
+
+    axios.get("http://localhost:8000/api/projects").then(Response => {
+      this.ProjectCard = Response.data.results;
       console.log(this.ProjectCard);
-    })
+    });
   },
 }
 </script>
 
 <template>
-  <div>
-    miao
+  <div class="" v-for="item in ProjectCard">
+    {{ item.title }}
+    {{ item.description }}
+    {{ item.slug }}
   </div>
 </template>
 
